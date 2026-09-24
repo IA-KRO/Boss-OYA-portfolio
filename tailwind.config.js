2cf7630349a924bbdcf6976f9f2ab3c65cf5f08d
IA-KRO <iakrosas2024@gmail.com>
2026-09-24 16:16:50 +0000

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   darkMode: "class",
-  theme: { extend: {} },
+  theme: {
+    extend: {},
+  },
   plugins: [],
 };
